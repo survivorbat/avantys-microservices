bceb016ea95680b448e670cf7a45c4c0aeca9b39
survivorbat <djbatcat@gmail.com>
2019-04-23 14:10:27 +0200

    develop: Add test controller

--- webapi/Controllers/StudentController.cs
+++ webapi/Controllers/StudentController.cs
@@ -6,6 +6,11 @@ namespace webapi.Controllers
     [ApiController]
     public class StudentController : Controller
     {
+        /// <summary>
+        /// A test endpoint to demonstrate that the API works
+        /// </summary>
+        /// <returns>{test: "This is a test response"}</returns>
+        /// <response code="200">{test: "This is a test response"}</response>
         [HttpGet]
         public IActionResult Index()
         {
