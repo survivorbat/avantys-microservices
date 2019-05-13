f9678ec21466d4e1b5e62e85e68138221f5269f1
Ruben Hensen <r.hensen@xenioss.io>
2019-05-13 09:25:11 +0200

    Base restful API tests

--- evaluating_students/index.js
+++ evaluating_students/index.js
@@ -2,6 +2,69 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true});
+
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+const TeacherSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    employeeNumber: Number,
+});
+
+let TeacherModel = mongoose.model('TeacherModel', TeacherSchema);
+
+const StudentSchema = new Schema({
+    firstName: String,
+    lastName: String,
+    studentNumber: Number,
+});
+
+let StudentModel = mongoose.model('TestModel', StudentSchema);
+
+const GradeSchema = new Schema({
+    student: Student,
+    grade: Number,
+    date: Date,
+});
+
+let GradeModel = mongoose.model('GradeModel', GradeSchema);
+
+const TestSchema = new Schema({
+    subject: ObjectId,
+    class: ObjectId,
+    enrolledStudents: [Student],
+    participatedStudents: [Student],
+    grades: [Grade],
+    testDate: Date,
+    gradingTeacher: Teacher
+});
+
+let Test = mongoose.model('Test', TestSchema);
+
+
+app.get('/tests', (req, res) => {
+
+    res.send('Hello World!')
+});
+
+app.put('/tests', (req, res) => res.send('Hello World!'));
+
+app.post('/tests', (req, res) => res.send('Hello World!'));
+
+app.delete('/tests', (req, res) => res.send('Hello World!'));
+
+
+app.get('/tests/{id}', (req, res) => res.send('Hello World!'));
+
+app.put('/tests/{id}', (req, res) => res.send('Hello World!'));
+
+app.post('/tests/{id}', (req, res) => res.send('Hello World!'));
+
+app.delete('/tests/{id}', (req, res) => res.send('Hello World!'));
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
