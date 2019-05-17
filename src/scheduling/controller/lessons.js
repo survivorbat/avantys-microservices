2a0c92aed347284c7861ebce2b79c14f61c05daf
Gerben Droogers <gerben@droogers.eu>
2019-05-17 13:38:13 +0200

    With pretier

--- src/scheduling/controller/lessons.js
+++ src/scheduling/controller/lessons.js
@@ -15,24 +15,23 @@ const getLessons = async (req, res, next) => {
 // const createLesson = async ({ body }, req, res) => res.status(200).json(body);
 
 const createLesson = async ({ body }, res, next) =>
-    await new Lessons(body, {})
-        .save()
-        .then(result => {
-            return res.redirect(303, "/api/v1/scheduling/lessons");
-        })
-        .catch(next);
+  await new Lessons(body, {})
+    .save()
+    .then(result => {
+      return res.redirect(303, "/api/v1/scheduling/lessons");
+    })
+    .catch(next);
 
 // const deleteLesson = async (req, res) => res.status(503).end();
 const deleteLesson = async ({ params: { id } }, res, next) =>
-    await Lessons.findOneAndDelete(id)
-        .then(result => {
-            return res.redirect(303, "/api/v1/scheduling/lessons");
-        })
-        .catch(next);
-
+  await Lessons.findOneAndDelete(id)
+    .then(result => {
+      return res.redirect(303, "/api/v1/scheduling/lessons");
+    })
+    .catch(next);
 
 module.exports = {
-    getLessons,
-    createLesson,
-    deleteLesson
+  getLessons,
+  createLesson,
+  deleteLesson
 };
