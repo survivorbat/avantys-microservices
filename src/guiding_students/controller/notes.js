fd577edbcc3b297783ef39a9c0adfb55e3a95816
Patrick <zammypatrick@gmail.com>
2019-05-21 10:26:36 +0200

    Feature/guiding students (#9)
    * added guding students models, controllers and events
    
    * changed line breaks
    
    * updated type in swagger
    
    * CRLF to LF
    
    * Creating notes is fixed
    
    * update to notes controller
    
    * deleted unecessary controllers

---
+++ src/guiding_students/controller/notes.js
@@ -0,0 +1,49 @@
+const Notes = require("../model/notes").Notes;
+const sendToQueue = require("../config/rabbitmq").connectAndSend;
+const NotesCreated = require("../events/NotesCreated");
+
+/**
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ * @returns {Promise<*>}
+ */
+const getAllNotes = async (req, res, next) =>
+  await Notes.find()
+    .then(notes =>
+      notes ? res.status(200).json(notes) : res.status(200).json([])
+    )
+    .catch(next);
+
+/**
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ * @returns {Promise<*>}
+ */
+const getNotes = async ({ params: { id } }, res, next) =>
+  await Notes.findById(id)
+    .then(notes => (notes ? res.status(200).json(notes) : res.end(404)))
+    .catch(next);
+
+/**
+ * @param {Object} req
+ * @param {Object} res
+ * @param {Function} next
+ * @returns {Promise<*>}
+ */
+const createNotes = async ({ body }, res, next) => 
+  await new Notes(body, {})
+    .save()
+    .then(result => {
+
+      sendToQueue(NotesCreated({ ...body, _id: result.id }));
+      return res.redirect(303, "notes");
+    })
+    .catch(next => console.log(next));
+
+module.exports = {
+  createNotes,
+  getAllNotes,
+  getNotes
+};
